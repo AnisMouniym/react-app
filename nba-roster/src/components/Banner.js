701e47f43ac36b1f93e43029f7bb3421cf1f255b
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-07-26 22:54:14 +0200

    css import and add nba logo

--- nba-roster/src/components/Banner.js
+++ nba-roster/src/components/Banner.js
@@ -1,5 +1,9 @@
+import '../styles/Banner.css'
+import logo from '../assets/NBA_logo.png'
 function Banner() {
-    return <h1>Create your NBA Roster</h1>
+    return <div className="banner">
+                <h1>Create your NBA Roster</h1>
+            </div>
 }
 
 export default Banner
