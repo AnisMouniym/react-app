93d1365c67132977c13df58d9bab2691005f34c9
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-07-29 13:42:14 +0200

    add stats

--- src/datas/playerList.js
+++ src/datas/playerList.js
@@ -4,70 +4,89 @@ export const playerList = [
 		position: 'point guard',
 		id: '1ed',
 		isMostChosen: true,
-		isHOF: false
+		isHOF: false,
+		rings: 4,
+		OVR: 96,
+		MVP: 2
 	},
 	{
 		name: 'RAY ALLEN',
 		position: 'shooting guard',
 		id: '2ab',
 		isMostChosen: false,
-		isHOF: true
-
+		isHOF: true,
+		rings: 2,
+		OVR: 95,
+		MVP: 0
 	},
 	{
 		name: 'JAMES HARDEN',
 		position: 'shooting guard',
 		id: '3sd',
 		isMostChosen: false,
-		isHOF: false
-
+		isHOF: false,
+		rings: 0,
+		OVR: 92,
+		MVP: 1
 	},
 	{
 		name: 'LEBRON JAMES',
 		position: 'forward',
 		id: '4kk',
 		isMostChosen: true,
-		isHOF: false
-
+		isHOF: false,
+		rings: 4,
+		OVR: 96,
+		MVP: 4
 	},
 	{
 		name: 'MICHAEL JORDAN',
 		position: 'shooting guard',
 		id: '5pl',
 		isMostChosen: true,
-		isHOF: true
-
+		isHOF: true,
+		rings: 6,
+		OVR: 98,
+		MVP: 5
 	},
 	{
 		name: 'KOBE BRYANT',
 		position: 'shooting guard',
 		id: '6uo',
 		isMostChosen: true,
-		isHOF: true
-
+		isHOF: true,
+		rings: 5,
+		OVR: 98,
+		MVP: 1
 	},
 	{
 		name: 'DAMIAN LILLARD',
 		position: 'point guard',
 		id: '7ie',
 		isMostChosen: false,
-		isHOF: false
-
+		isHOF: false,
+		rings: 0,
+		OVR: 90,
+		MVP: 0
 	},
 	{
 		name: 'KEVIN DURANT',
 		position: 'forward',
 		id: '8fp',
 		isMostChosen: false,
-		isHOF: false
-
+		isHOF: false,
+		rings: 2,
+		OVR: 96,
+		MVP: 1	
 	},
 	{
 		name: 'TIM DUNCAN',
 		position: 'forward',
 		id: '9vn',
 		isMostChosen: false,
-		isHOF: true
-
+		isHOF: true,
+		rings: 5,
+		OVR: 99,
+		MVP: 2
 	}
 ]
